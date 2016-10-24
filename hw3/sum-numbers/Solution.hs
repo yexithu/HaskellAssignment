730a65f355ac1dd8f7a42dc3bb8e9a28d45d7ef7
yexithu <502000201@qq.com>
2016-10-24 20:40:40 +0800

    clean hw1

--- hw3/sum-numbers/Solution.hs
+++ hw3/sum-numbers/Solution.hs
@@ -7,8 +7,6 @@ solution [] = 0
 solution x = foldl (+) 0 (map myParseInt (filter isStrNumber (words x)))
 
 myParseInt :: String -> Int
---myParseInt [] = 0
---myParseInt xs = foldl (\acc x -> 10 * acc + x) 0 xs
 myParseInt [] = 0
 myParseInt x = read x :: Int
 
