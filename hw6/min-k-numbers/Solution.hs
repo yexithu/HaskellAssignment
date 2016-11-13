95c4e12e12104377ce4e11a05e9c9efd75133ab6
yexithu <yexithu14@163.com>
2016-11-14 02:21:08 +0800

    Hw6

---
+++ hw6/min-k-numbers/Solution.hs
@@ -0,0 +1,16 @@
+module Solution where
+
+import Data.List
+
+solution :: [Integer] -> Int -> [Integer]
+solution _  0 = []
+solution x k 
+    | (2 * k) <= l = solution' x k mid
+    | otherwise = take k $ sort x 
+    where l = length x
+          mid = l `div` 2
+
+solution' :: [Integer] -> Int -> Int -> [Integer]
+solution' x k mid = 
+    let (a, b) = splitAt mid x
+    in take k $ sort $ (solution a k) ++ (solution b k)
---
+++ hw6/queens/Solution.hs
@@ -0,0 +1,31 @@
+module Solution where
+import Data.List
+import Data.Char
+solution :: [(Char, Int)] -> Int
+solution [] = 8
+solution x = solution' $ map charToInt x
+
+solution' :: [(Int, Int)] -> Int
+solution' x = let l = length x
+                  board = [(a, b) | a <- [1,2..8], b <- [1,2..8]]
+                  left = foldl (\z ele -> filter (isSafe ele) z) board x
+              in (solutionInt l left) - l
+
+charToInt :: (Char, Int) -> (Int, Int)
+charToInt (a, b) = (ord(a) - ord('a') + 1, b) 
+
+solutionInt :: Int -> [(Int, Int)] -> Int
+solutionInt x [] = x
+solutionInt x left = foldl (\z ele -> bfsearch x left z ele) 0 left
+  
+bfsearch :: Int -> [(Int, Int)] -> Int -> (Int, Int) -> Int
+bfsearch x left z ele = let removed = filter (isSafe ele) left
+                            y = solutionInt (x + 1) removed
+                        in if y > z then y else z
+
+isSafe :: (Int, Int) -> (Int, Int) -> Bool
+isSafe (a, b) (c, d)
+    | a == c = False
+    | b == d = False
+    | (abs (a - c)) == (abs (b - d)) = False
+    | otherwise = True
